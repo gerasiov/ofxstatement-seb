1a6cdd5ad5f4e9e84c65d3474a475c711589a9e3
Alexander Krasnukhin <the.malkolm@gmail.com>
2016-04-18 00:27:55 +0200

    Use click.

--- convert.py
+++ convert.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
 import os.path
+import click
 
 from ofxstatement.ofx import OfxWriter
 from ofxstatement.plugins import seb
 
 
-def main(args=None):
-    parser = argparse.ArgumentParser(
-        description='Parse and print transactions from SEB export xlsx file.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    parser.add_argument('path', help='path to the xlsx file')
-    parser.add_argument('--dump', action='store_true', help='print statements')
-    opts = parser.parse_args(args)
+@click.command()
+@click.argument('path')
+@click.option('--debug', is_flag=True, default=False)
+def convert(path, debug):
+    """Parse and print transactions from SEB export xlsx file."""
 
-    input_file = opts.path
-    root, ext = os.path.splitext(input_file)
+    root, ext = os.path.splitext(path)
     output_file = root + '.ofx'
-    parser = seb.SebPlugin(None, None).get_parser(opts.path)
+
+    parser = seb.SebPlugin(None, None).get_parser(path)
     statement = parser.parse()
 
-    if opts.dump:
+    if debug:
         for line in statement.lines:
             print(line)
         return
+
     with open(output_file, 'w') as out:
         writer = OfxWriter(statement)
         out.write(writer.toxml())
 
 
 if __name__ == '__main__':
-    main()
+    convert()
